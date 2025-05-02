372c33e0de962e90160355e7e0a9710ee44f8ff2
sanju.shaji <sanjucrz007@gmail.com>
2025-05-02 09:59:24 +0530

    again 1

--- demo/src/main/java/com/example/demo/dto/AssignDoctorToPatientDTO.java
+++ demo/src/main/java/com/example/demo/dto/AssignDoctorToPatientDTO.java
@@ -3,6 +3,6 @@ package com.example.demo.dto;
 
 public class AssignDoctorToPatientDTO {
 void test(){
-    System.out.println("helldwfsdfdsrwerewrffsdffooddo");
+    System.out.println("hellewrffsdffooddo");
 }
 }
